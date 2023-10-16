549a400078acb397c3ca3af6ea9ddba9eae681b6
Tafsir GNA <gnatafsir@gmail.com>
2023-10-16 21:51:57 +0100

    License and credits page improvements

--- browser_extensions/linkbeam-chrome/src/react_components/LicenseCredits.jsx
+++ browser_extensions/linkbeam-chrome/src/react_components/LicenseCredits.jsx
@@ -15,6 +15,7 @@ export default class LicenseCreditsView extends React.Component{
   constructor(props){
     super(props);
     this.state = {
+      currentTabIndex: 0,
     };
   }
 
@@ -24,10 +25,52 @@ export default class LicenseCreditsView extends React.Component{
 
   }
 
+  switchCurrentTab(index){
+
+    this.setState({currentTabIndex: index});
+
+  }
+
   render(){
     return (
       <>
-        
+        <div class="p-3">
+          <BackToPrev prevPageTitle="About"/>
+
+          <div class="text-center my-3">
+            <div class="btn-group btn-group-sm mb-2 shadow-sm" role="group" aria-label="Small button group">
+              <button type="button" class={"btn btn-primary badge" + (this.state.currentTabIndex == 0 ? " active " : "")} onClick={() => {this.switchCurrentTab(0)}}>
+                Credits
+              </button>
+              <button type="button" class={"btn btn-secondary badge" + (this.state.currentTabIndex == 1 ? " active " : "") } title="See Credits" onClick={() => {this.switchCurrentTab(1)}} >
+                License
+              </button>
+            </div>
+          </div>
+
+          <div class="fw-light small border border-secondary-subtle rounded p-3 shadow">
+
+          { this.state.currentTabIndex == 0 && <div class="row mx-1">
+
+                                                <p class="col text-end fw-bold">Resources</p>
+
+                                                <p class="col">Icons8</p>
+
+                                              </div>}
+
+          { this.state.currentTabIndex == 1 && <div class="">
+
+                                                <p>Copyright (C) 2023 Tafsir GNA</p>
+
+                                                <p>This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.</p>
+
+                                                <p>This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.</p>
+
+                                              </div>}
+
+          </div>
+
+        </div>
       </>
     );
   }
